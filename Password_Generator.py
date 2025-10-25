76f04abb372b4c2251c834e7a327237be3e68c69
Shivansh Sharma <157245723+Shivansh-Game@users.noreply.github.com>
2025-10-25 21:43:51 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -14,7 +14,8 @@ def get_password(length):
     letters_upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     numbers="1234567890"
     symbols="!@#$%^&*()_-+="
-    password_chars=[
+    all_chars=letters_lower+letters_upper+numbers+symbols
+    password_chars=[ # although it is slightly worse for security I have decided to keep this for the case that the user wants a password of length 4 as it'd save time in that case 
         random.choice(letters_lower),
         random.choice(letters_upper),
         random.choice(numbers),
@@ -23,8 +24,7 @@ def get_password(length):
     if length<4:
         print(f"{RED}Password length should be atleast 4 to include all character types.{RESET}")
         return None
-    all_chars=letters_lower+letters_upper+numbers+symbols
-    for _ in range(length-4):
+    for _ in range(length - 4):
         password_chars.append(random.choice(all_chars))
     random.shuffle(password_chars)
     password="".join(password_chars)
@@ -38,34 +38,31 @@ def password_checker(password):
     has_digit = any(c.isdigit() for c in password)
     has_symbol = any(c in "!@#$%^&*()_-+=" for c in password)
 
-    score = 0
+    score = 3
     if length > 8:
         score += 1
     if length > 12:
         score += 1
-    if has_lower and has_upper:
-        score += 1
-    if has_digit:
-        score += 1
-    if has_symbol:
-        score += 1
+    # all of these if conditions are redundant as the password is hardcoded to meet all of these conditions to begin with, simply initialize score to 3 
+    #if has_lower and has_upper:
+    #    score += 1
+    #if has_digit:
+    #    score += 1
+    #if has_symbol:
+    #    score += 1
 
-    if score <= 2:
-        return f"{RED}Weak{RESET}"
-    elif score in [3, 4]:
-        return f"{YELLOW}Medium{RESET}"
-    else:
-        return f"{GREEN}Strong{RESET}"
+    return score
 
 #Main Function
 def main():
     try:
-        length=int(input("Enter length of your password: "))
+        length=int(input("Enter length of your password (12 or above is recommended, 4 is the minimum): "))
         password = get_password(length)
         if password:
-            print(f"\n{CYAN}Your generated password is: {RESET}{password}")
             strength = password_checker(password)
-            print(f"Password strength: {strength}\n")
+            print(f"Password strength: {strength}/5 \n")
+            print(f"\n{CYAN}Your generated password is: {RESET}{password}")
+            
             copy = input("Do you want to copy password?(yes/no):")
             if copy.lower() in ['yes' or 'y']:
                 pyperclip.copy(password)
@@ -81,4 +78,5 @@ if __name__=="__main__":
         again=input("Do you want to generate another password?(yes/no): ")
         if again.lower() != 'yes':
             print(f"{CYAN}Exiting Password Generator{RESET}")
+
             break
